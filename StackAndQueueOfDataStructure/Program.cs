137ee556532d5ba489b694edcf34d3f90d8303ff
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-05-03 08:42:33 +0530

    [VINAY] Add: Pushing Elements in the Stack

---
+++ StackAndQueueOfDataStructure/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using StackAndQueueOfDataStructure;
+class Program
+{
+    public static void Main(String[] args)
+    { 
+    StackAndQueue stack=new StackAndQueue();
+        stack.Push(70);
+        stack.Push(30);
+        stack.Push(56);
+        stack.Display();
+    }
+}
---
+++ StackAndQueueOfDataStructure/StackAndQueue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueOfDataStructure
+{
+    public class StackAndQueue
+    {
+        private Node top;
+        public StackAndQueue()
+        {
+            this.top =null;
+        }
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack" + value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
